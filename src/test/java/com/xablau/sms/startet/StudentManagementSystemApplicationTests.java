969ec785776d8b25c8e885c50c5b2b2e3707d0fb
Matheus <matheus_adps@hotmail.com>
2021-10-21 20:41:23 -0300

    Projeto spring de gerenciamento de alunos

---
+++ src/test/java/com/xablau/sms/startet/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xablau.sms.startet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
